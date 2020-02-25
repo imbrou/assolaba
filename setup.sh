46aab327ab91ed7fe9045d9be724b730455c885c
Nicolas Broussard <nicolas@skale-5.com>
2020-02-25 22:26:20 +0100

    added docker-compoe install

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@
 echo Installing docker
 sudo apt update
 sudo apt install -y docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo Setting up docker
 sudo systemctl enable docker
